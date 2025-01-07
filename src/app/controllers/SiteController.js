3a072d0f927604e739726e8555bed051154e2f4b
sherryvippro <sherryvippro@gmail.com>
2025-01-07 10:18:11 +0700

    read data from db

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -1,12 +1,15 @@
 const Course = require('../models/Course')
+const { mutipleMongooseToObject } = require('../../util/mongoose')
 
 class SiteController {
     // [GET] /
     home(req, res) {
         Course.find({})
-            .then((courses) => {
-                res.json(courses)
-            })
+            .then((courses) =>
+                res.render('home', {
+                    courses: mutipleMongooseToObject(courses),
+                }),
+            )
             .catch((err) => {
                 res.status(400).json({ error: 'Internal Server Error' })
             })
